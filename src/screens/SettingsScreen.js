e1f0d371d5c0e9d4f30eb0f5f4fe1545c85e1c47
JKobrynski <kobrynski@onet.pl>
2019-12-21 17:48:47 +0100

    Almost ready

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useState} from 'react';
 import {
   View,
   Text,
@@ -12,85 +12,35 @@ import {SafeAreaView} from 'react-navigation';
 import {colors} from '../constants/colors';
 import {vh, vw} from '../constants/sheet';
 import Icon from 'react-native-vector-icons/Ionicons';
-import {TextInput} from 'react-native-gesture-handler';
 import UIButton from '../components/UIButton';
 import {passwordConfig} from './LoginScreen';
 import SecureStorage from 'react-native-secure-storage';
 
 const SettingsScreen = ({navigation}) => {
-  const [previous, setPrevious] = useState('');
-  const [correct, setCorrect] = useState(false);
-  const [updated, setUpdated] = useState('');
   const [success, setSuccess] = useState(false);
-  const [error, setError] = useState('');
-  const [visible, setVisible] = useState(true);
   const [pending, setPending] = useState(false);
-
-  const _saved = useRef(null);
-  const _updated = useRef(null);
-
-  // Usuwanie błędu jeśli hasło jest ponownie wprowadzane
-  useEffect(() => {
-    if (error && previous.length > 0) {
-      setError('');
-    }
-  }, [previous]);
-
-  // Usuwanie błędu jeśli hasło jest ponownie wprowadzane
-  useEffect(() => {
-    if (error && updated.length > 0) {
-      setError('');
-    }
-  }, [updated]);
-
-  // Działanie po udanej zmianie hasła
-  useEffect(() => {
-    if (success) {
-      const {current} = _updated;
-
-      // Zamknięcie aktywnego inputu (unfocus)
-      if (current) {
-        current.blur();
-      }
-
-      // Wyczyszczenie inputu
-      setUpdated('');
-
-      // Schowanie przycisku
-      setVisible(false);
-    }
-  }, [success]);
+  const [error, setError] = useState('');
 
   const keyboardVerticalOffset = Platform.OS === 'ios' ? 40 : 0;
 
-  // Metoda porównująca wpisane hasło z zapisanym
-  const _checkPassword = async () => {
-    // setPending(true);
-    // try {
-    //   // Pobranie hasła z pamięci urządzenia
-    //   const savedPassword = await SecureStorage.getItem('@password', config);
-    //   // Porównanie zapisanego hasła z zapisanym hasłem
-    //   bcrypt.compare(previous, savedPassword, (err, matched) => {
-    //     if (err) {
-    //       setError(err);
-    //     }
-    //     if (matched) {
-    //       setCorrect(true);
-    //     } else {
-    //       setError('Hasło nieprawidłowe');
-    //       setPrevious('');
-    //     }
-    //   });
-    // } catch (err) {
-    //   setError('Wystąpił błąd podczas odczytu danych');
-    // } finally {
-    //   setPending(false);
-    // }
-  };
+  const _changePassword = async () => {
+    try {
+      setPending(true);
 
-  // Metoda zapisująca nowe hasło w pamięci urządzenia
-  const _updatePassword = () => {
-    //
+      const note = await SecureStorage.getItem('@note2', passwordConfig);
+
+      if (note) {
+        await SecureStorage.removeItem('@note2', passwordConfig);
+        await SecureStorage.setItem('@note2', note, passwordConfig);
+        setSuccess(true);
+      } else {
+        setError('Nie posiadasz jeszcze hasła');
+      }
+    } catch (err) {
+      setError('Wystąpił błąd przy próbie zmiany hasła');
+    } finally {
+      setPending(false);
+    }
   };
 
   return (
@@ -99,94 +49,33 @@ const SettingsScreen = ({navigation}) => {
         behavior={Platform.OS === 'ios' ? 'padding' : null}
         style={{flex: 1}}
         keyboardVerticalOffset={keyboardVerticalOffset}>
+        <View style={styles.header}>
+          <TouchableOpacity
+            style={styles.backButton}
+            onPress={() => navigation.navigate('Login')}>
+            <Icon name="ios-arrow-back" color="#fff" size={5 * vh} />
+          </TouchableOpacity>
+        </View>
         <View style={styles.container}>
-          <View style={styles.upperWrapper}>
-            <View style={styles.header}>
-              <TouchableOpacity
-                style={styles.backButton}
-                onPress={() => navigation.navigate('Login')}>
-                <Icon name="ios-arrow-back" color="#fff" size={5 * vh} />
-              </TouchableOpacity>
-            </View>
-            <Text style={styles.bigText}>Zmień hasło</Text>
-            {success ? (
-              <Text style={styles.successMessage}>
-                Hasło zostało zaktualizowane
-              </Text>
-            ) : null}
-          </View>
-          {success ? (
-            <Icon
-              name="ios-checkmark"
-              size={12 * vh}
-              color={colors.secondary}
-            />
-          ) : (
-            <View style={{width: '80%'}}>
-              <Text style={styles.smallText}>
-                {correct ? 'Podaj nowe hasło' : 'Podaj obecne hasło'}
-              </Text>
-              {correct ? (
-                <>
-                  <TextInput
-                    ref={_updated}
-                    style={{
-                      borderBottomColor: success
-                        ? colors.secondary
-                        : colors.primaryVariant,
-                      ...styles.input,
-                    }}
-                    value={updated}
-                    onChangeText={setUpdated}
-                    secureTextEntry
-                    fontSize={3 * vh}
-                    color="#fff"
-                    returnKeyType="next"
-                    onSubmitEditing={_updatePassword}
-                    autoFocus
-                  />
-                  <Text style={styles.info}>
-                    Przynajmniej 8 znaków, 2 duze litery i 2 cyfry
-                  </Text>
-                </>
-              ) : (
-                <TextInput
-                  ref={_saved}
-                  style={{
-                    borderBottomColor: error
-                      ? colors.error
-                      : colors.primaryVariant,
-                    ...styles.input,
-                  }}
-                  value={previous}
-                  onChangeText={setPrevious}
-                  secureTextEntry
-                  fontSize={3 * vh}
-                  color="#fff"
-                  autoFocus
-                  returnKeyType="next"
-                  onSubmitEditing={_checkPassword}
-                  blurOnSubmit={false}
-                />
-              )}
+          <View style={styles.midWrapper}>
+            <View style={styles.upperWrapper}>
               {pending ? (
-                <ActivityIndicator
-                  size="large"
-                  color={colors.primaryVariant}
-                  style={{marginTop: 2 * vh}}
-                />
-              ) : null}
-              {error ? <Text style={styles.error}>{error}</Text> : null}
+                <ActivityIndicator color={colors.primaryVariant} size="large" />
+              ) : (
+                <Text style={styles.bigText}>Zmień hasło</Text>
+              )}
             </View>
-          )}
-          <View style={styles.buttonContainer}>
-            {visible ? (
+            {success ? (
+              <Text style={styles.successMessage}>Hasło zostało zmienione</Text>
+            ) : null}
+            {error ? <Text style={styles.error}>{error}</Text> : null}
+            <View style={styles.buttonContainer}>
               <UIButton
                 color={colors.primaryVariant}
-                label={correct ? 'Zapisz' : 'Dalej'}
-                onPress={correct ? _updatePassword : _checkPassword}
+                label={'Dalej'}
+                onPress={_changePassword}
               />
-            ) : null}
+            </View>
           </View>
         </View>
       </KeyboardAvoidingView>
@@ -203,14 +92,15 @@ const styles = StyleSheet.create({
     width: '100%',
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between',
     paddingTop: 4 * vh,
   },
   upperWrapper: {
     width: '100%',
+    alignItems: 'center',
   },
   header: {
     paddingHorizontal: 4 * vw,
+    marginTop: 6 * vw,
     flexDirection: 'row',
     justifyContent: 'flex-start',
   },
@@ -223,22 +113,18 @@ const styles = StyleSheet.create({
     fontSize: 4 * vh,
     textAlign: 'center',
   },
+  midWrapper: {
+    width: '100%',
+    height: '50%',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
   successMessage: {
     fontFamily: 'Montserrat-Regular',
-    fontSize: 2 * vh,
+    fontSize: 5 * vw,
     color: colors.secondary,
     textAlign: 'center',
     marginTop: 1 * vh,
-  },
-  smallText: {
-    fontFamily: 'Montserrat-Regular',
-    color: colors.onBackground,
-    fontSize: 3 * vh,
-    marginBottom: 3 * vh,
-  },
-  input: {
-    borderBottomWidth: 2,
-    textAlign: 'center',
   },
   error: {
     color: colors.error,
@@ -250,12 +136,6 @@ const styles = StyleSheet.create({
   buttonContainer: {
     width: '80%',
     height: 8 * vh,
-  },
-  info: {
-    color: '#fff',
-    fontFamily: 'Montserrat-Regular',
-    fontSize: 2 * vh,
-    marginTop: 1 * vh,
   },
 });
 
